b75d4e3fbd0b46294a2cb83e579e2a05dfb3c4a0
Ian Lancaster <ianclancaster@gmail.com>
2017-02-04 16:02:56 -0700

    Builds top of fold homepage content

--- src/routes/Home/Home.Component.js
+++ src/routes/Home/Home.Component.js
@@ -1,3 +1,5 @@
+const headShotImg = require('./assets/Ian-Lancaster-Headshot.jpg')
+
 import React, { Component, PropTypes } from 'react'
 import classes from './Home.styles.scss'
 
@@ -16,9 +18,26 @@ class Home extends Component {
     const { content, title } = this.props
 
     return (
-      <div className={classes.homeBackground}>
-        <h2>{title}</h2>
-        <section dangerouslySetInnerHTML={{ __html: content }} />
+      <div>
+        <section className={classes.homeBackground}>
+          <div className={classes.mainContent}>
+            <div className={classes.columnOneThird}>
+              <img src={headShotImg} className={classes.headShotImg} />
+            </div>
+            <span className={classes.columnTwoThirds}>
+              <h2>Hello, My name is Ian.</h2>
+              <p>
+                I am a front end software engineer and search engine optimization specialist.
+              </p>
+            </span>
+          </div>
+        </section>
+        <section className={classes.mainContent}>
+          <span className={classes.columnOneThird} />
+          <span className={classes.columnTwoThirds}>
+            <span dangerouslySetInnerHTML={{ __html: content }} />
+          </span>
+        </section>
       </div>
     )
   }
