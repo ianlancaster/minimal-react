d1551346b8f1e3d300165532efcfe682e7c984d8
Ian Lancaster <ianclancaster@gmail.com>
2017-02-07 12:50:55 -0700

    SI local host is up

--- index.php
+++ index.php
@@ -6,15 +6,16 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width,initial-scale=1">
   <?php wp_head(); ?>
-  <link rel="shortcut icon" href="http://192.168.0.8/searchinnovator.com/wp-content/themes/minimal-react/dist/favicon.ico">
-  <link href="http://192.168.0.8/searchinnovator.com/wp-content/themes/minimal-react/dist/app.css" rel="stylesheet">
+  <link rel="shortcut icon" href="/searchinnovator.com/wp-content/themes/minimal-react/dist/favicon.ico">
+  <link href="/searchinnovator.com/wp-content/themes/minimal-react/dist/app.css" rel="stylesheet">
 </head>
 
 <body>
+  <h1>the index template has rendered</h1>
   <div id="root" style="height: 100%"></div>
   <?php wp_footer(); ?>
-  <script type="text/javascript" src="http://192.168.0.8/searchinnovator.com/wp-content/themes/minimal-react/dist/vendor.js"></script>
-  <script type="text/javascript" src="http://192.168.0.8/searchinnovator.com/wp-content/themes/minimal-react/dist/app.js"></script>
+  <script type="text/javascript" src="/searchinnovator.com/wp-content/themes/minimal-react/dist/vendor.js"></script>
+  <script type="text/javascript" src="/searchinnovator.com/wp-content/themes/minimal-react/dist/app.js"></script>
 </body>
 
 </html>
